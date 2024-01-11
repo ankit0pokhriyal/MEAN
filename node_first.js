b0cc9613f30e69be1f3b0a84785b133caad43778
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-01-11 13:01:32 +0530

    Add files via upload

---
+++ node_first.js
@@ -0,0 +1,14 @@
+console.log("Let's start") // Use for print
+//const fs = require('fs');
+//const os = require('os');
+// fs.appendFile("First Text", "The End")
+/*fs.appendFile("First Text", "The END", function(err){
+    if (err){
+        console.log("Can't Write");
+    }
+
+});*/
+//var user = os.userInfo();
+//fs.appendFileSync("first Text", 'The End\n'+ `hi ${user.username}`); // Use to write files 'File System'
+//console.log(user)
+// console.log(module)
